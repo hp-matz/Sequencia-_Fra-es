cd5c02c8618ff2d75e68eaf31777baac024bcf54
MATZ_HP <mattnohype@gmail.com>
2020-10-27 23:52:53 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int x = 6;
+  int z = 1;
+  int y;
+  int a = 4;
+  int b = 2;
+  int c;
+
+  printf("(4/4) ");
+
+  while ( y < 100 || c <68) {
+
+    y = x + z;
+      printf( "+ (%d/", y);
+    z = z + 3;
+
+    c = a + b;
+      printf( "%d) ", c);
+    b = b + 2;
+  }
+  return 0;
+}
